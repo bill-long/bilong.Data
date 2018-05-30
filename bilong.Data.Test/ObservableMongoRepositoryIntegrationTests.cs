bdd6cb9350276be82d26760500e978857dd0097d
Bill Long (CPR) <bilong@microsoft.com>
2018-05-30 13:36:08 -0500

    Test with multiple observers

--- bilong.Data.Test/ObservableMongoRepositoryIntegrationTests.cs
+++ bilong.Data.Test/ObservableMongoRepositoryIntegrationTests.cs
@@ -36,23 +36,26 @@ namespace bilong.Data.Test
 
             // Now we can start watching for changes
             const int numberOfChanges = 100;
-            var observer = new RepoObserver(_output);
-            using (var unsubscriber = repo.Subscribe(observer))
+            var observer1 = new RepoObserver("o1", _output);
+            var observer2 = new RepoObserver("o2", _output);
+            using (var unsub1 = repo.Subscribe(observer1))
+            using (var unsub2 = repo.Subscribe(observer2))
             {
                 // Act
                 var range = Enumerable.Range(0, numberOfChanges);
-                Parallel.ForEach(range, new ParallelOptions {MaxDegreeOfParallelism = 500}, i =>
-                {
-                    var newEntity = repo.Add(new TestEntity {Name = i.ToString(), Id = i.ToString()}, "testUserId")
-                        .GetAwaiter().GetResult();
-                });
+                Parallel.ForEach(range, new ParallelOptions { MaxDegreeOfParallelism = 500 }, i =>
+                  {
+                      var newEntity = repo.Add(new TestEntity { Name = i.ToString(), Id = i.ToString() }, "testUserId")
+                          .GetAwaiter().GetResult();
+                  });
 
                 // Allow time for all changes to come through
                 Thread.Sleep(5000);
             }
 
             // Assert
-            Assert.Equal(numberOfChanges, observer.NumberOfCallsToOnNext);
+            Assert.Equal(numberOfChanges, observer1.NumberOfCallsToOnNext);
+            Assert.Equal(numberOfChanges, observer2.NumberOfCallsToOnNext);
         }
     }
 
@@ -72,27 +75,29 @@ namespace bilong.Data.Test
 
     public class RepoObserver : IObserver<RepositoryChange<TestEntity>>
     {
+        private readonly string _name;
         private readonly ITestOutputHelper _output;
         public int NumberOfCallsToOnNext = 0;
 
-        public RepoObserver(ITestOutputHelper output)
+        public RepoObserver(string name, ITestOutputHelper output)
         {
+            _name = name;
             _output = output;
         }
 
         public void OnCompleted()
         {
-            _output.WriteLine("RepoObserver.OnCompleted");
+            _output.WriteLine($"RepoObserver.OnCompleted {_name}");
         }
 
         public void OnError(Exception error)
         {
-            _output.WriteLine($"RepoObserver.OnError {error}");
+            _output.WriteLine($"RepoObserver.OnError {_name} {error}");
         }
 
         public void OnNext(RepositoryChange<TestEntity> value)
         {
-            _output.WriteLine($"RepoObserver.OnNext {value.OpType} {value.Item}");
+            _output.WriteLine($"RepoObserver.OnNext {_name} {value.OpType} {value.Item}");
             Interlocked.Increment(ref NumberOfCallsToOnNext);
         }
     }
